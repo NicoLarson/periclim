f1b42c71898173f5766951483d66f0a282308eac
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-05-24 08:40:52 -0300

    Updates

---
+++ precache-manifest.d4a3ffa7cf6546582e6c26e373a870c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "006cb938171e2a46aefe",
+    "url": "/periclim/static/css/main.71d2c19b.chunk.css"
+  },
+  {
+    "revision": "006cb938171e2a46aefe",
+    "url": "/periclim/static/js/main.006cb938.chunk.js"
+  },
+  {
+    "revision": "b9b632de90659299e5fc",
+    "url": "/periclim/static/js/1.b9b632de.chunk.js"
+  },
+  {
+    "revision": "fdfc9cb78cf98fedca44",
+    "url": "/periclim/static/js/runtime~main.fdfc9cb7.js"
+  },
+  {
+    "revision": "d4df5b006accfad8a36e1f6a9154292a",
+    "url": "/periclim/index.html"
+  }
+];
